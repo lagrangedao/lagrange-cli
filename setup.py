d9f3faf041ee9944affb67ff256034e91b6af344
Fei Lin <fei.lin@nbai.io>
2023-05-04 09:03:49 -0600

    Update package reqs

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='lagrange-cli',
+    install_requires= [
+        "requests>=2.28.0"
+    ],
     version='1.0',
     description='A CLI tool that allows you to interact with datasets, models, and spaces all from the command line.',
     entry_points={
